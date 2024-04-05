02c1d3c1eb77a727640af06526dfc849aebe94f2
moryproject <moryproject12@gmail.com>
2024-04-05 16:30:55 +0700

    try commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          trying to commit
         </a>
       </header>
     </div>
